13cd18a8a2a678c5b17d8b43521e76b80dee4373
0xe1a00000 <m.gebele@tuta.io>
2019-05-04 13:26:23 +0200

    Changed a bit

--- anti-fool-disas.py
+++ anti-fool-disas.py
@@ -1,14 +1,25 @@
-#!/usr/bin/env python3
 # ----------------------------------------------------------------------------
-# anti-fool-disas.py
+# anti-fool-disas.py - Written by Manuel Gebele (m.gebele@tuta.io)
 #
 # An attempt to improve function detection such as tail-calls and
 # address-taken functions.
 #
 # Inspired by the book "Practical Binary Analysis" by Dennis Andriesse.
+# ----------------------------------------------------------------------------
+# Copyright (C) 2019 Manuel Gebele |0xe1a00000|
 #
-# NOP (0xe1a00000@tuta.io)
-# This code is in the public domain.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # ----------------------------------------------------------------------------
 import sys
 import collections
